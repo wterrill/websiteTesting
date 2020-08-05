d38e5251cb03b06931b010ccb39bde4069b442b9
William Terrill <william.terrill@gmail.com>
2020-08-04 21:09:37 -0500

    AutoCommit webTesting Tue Aug  4 21:09:37 CDT 2020

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "63aaaa5fc1bfefb67446070a68e81d2b",
 "/": "63aaaa5fc1bfefb67446070a68e81d2b",
-"main.dart.js": "1776ee971c9954213fc2cc63e57e6f55",
+"main.dart.js": "470617c8bc4f29edb22d950eb9539526",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
